89deed21b4e787231207d0429b82a231f688a58b
gabrielforster <rochafrgabriel@gmail.com>
2022-11-24 08:38:39 -0300

    feat: add eslint -> lint code

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser"
-import * as url from "url"
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+import bodyParser from "body-parser";
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-const app = express()
-app.use(express.json())
-app.use(express.static(__dirname + "/app"))
-app.use(bodyParser.urlencoded({ extended: false }))
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname + "/app"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-		res.sendFile(__dirname + "/app/index.html")
+  res.sendFile(__dirname + "/app/index.html");
 });
 
 app.listen(3000, () => {
-	console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
